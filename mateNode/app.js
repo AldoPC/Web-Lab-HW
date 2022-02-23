1e9b12e3e3cecfaf4a42e2ecc936b2b102d0978b
Aldo Ponce <aldoponcedelacruz@gmail.com>
2022-02-23 11:15:52 -0600

    Update

--- mateNode/app.js
+++ mateNode/app.js
@@ -1,29 +1,26 @@
 let mateBasica = require("./mate");
 let prompt = require("prompt-sync")();
 
-let opcion = prompt(
-  `Operaciones: 1)Suma 2)Resta 3)Multiplicacion 4)Division 5)Modulo
-`
-);
-let num1 = prompt(`Ingrese el primer numero
-`);
-let num2 = prompt(`Ingrese el segundo numero
-`);
+let opcion = Number(prompt(
+`Operaciones: 1)Suma 2)Resta 3)Multiplicacion 4)Division 5)Modulo: `
+));
+let num1 = Number(prompt(`Ingrese el primer numero: `));
+let num2 = Number(prompt(`Ingrese el segundo numero: `));
 
-switch (Number(opcion)) {
+switch (opcion) {
   case 1:
-    console.log(mateBasica.suma(Number(num1), Number(num2)));
+    console.log(mateBasica.suma(num1, num2));
     break;
   case 2:
-    console.log(mateBasica.resta(Number(num1), Number(num2)));
+    console.log(mateBasica.resta(num1, num2));
     break;
   case 3:
-    console.log(mateBasica.producto(Number(num1), Number(num2)));
+    console.log(mateBasica.producto(num1, num2));
     break;
   case 4:
-    console.log(mateBasica.division(Number(num1), Number(num2)));
+    console.log(mateBasica.division(num1, num2));
     break;
   case 5:
-    console.log(mateBasica.modulo(Number(num1), Number(num2)));
+    console.log(mateBasica.modulo(num1, num2));
     break;
 }
